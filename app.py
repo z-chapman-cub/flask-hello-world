1f360d4bc76df5c95949df5d4fe889cf10c22ef9
z-chapman-cub <Zane.Chapman@Colorado.edu>
2025-11-11 17:15:15 +0000

    psycopg2-binary changed to 2.9.11

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
   
 @app.route('/db_test')
 def testing():
-  conn = psycopg2.connect("postgresql://lab10db_60s2_user:NyfDB7KMgh2UYpg6bU4Udx668ao7ZeaZ@dpg-d49mmv9e2q1c73dn6j40-a/lab10db_60s2")
-  conn.close()
-  return "Database Connection Successful"
+    conn = psycopg2.connect("postgresql://lab10db_60s2_user:NyfDB7KMgh2UYpg6bU4Udx668ao7ZeaZ@dpg-d49mmv9e2q1c73dn6j40-a/lab10db_60s2")
+    conn.close()
+    return "Database Connection Successful"
