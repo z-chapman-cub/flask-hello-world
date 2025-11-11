df733469ecf55f19b5d65e74235910ba4ffeac0a
z-chapman-cub <Zane.Chapman@Colorado.edu>
2025-11-11 17:24:48 +0000

    db_create route added

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
     conn = psycopg2.connect("postgresql://lab10db_60s2_user:NyfDB7KMgh2UYpg6bU4Udx668ao7ZeaZ@dpg-d49mmv9e2q1c73dn6j40-a/lab10db_60s2")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgresql://lab10db_60s2_user:NyfDB7KMgh2UYpg6bU4Udx668ao7ZeaZ@dpg-d49mmv9e2q1c73dn6j40-a/lab10db_60s2")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
